7295d00cb757d665b9e7c8cf5d535f60f189b066
ruifan831 <46877205+ruifan831@users.noreply.github.com>
2020-08-11 21:19:48 -0700

    Add files via upload

---
+++ eventListener.js
@@ -0,0 +1,9 @@
+
+// in the Google Form when the Submit button is clicked, storeTheForm function will triggered.
+const submitBTN = document.getElementById("submit")
+
+submitBTN.addEventListener('click', storeTheForm, false);
+
+const storeTheForm = () =>{
+    var data = document.getElementById("submitForm")
+}
